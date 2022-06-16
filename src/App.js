7bff990e536ffcf3554c1cb3a03fe558ef4673e4
Michael Bohn <michael.bohn@freiheit.com>
2022-06-16 22:05:41 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import { Stage, Layer, Line } from 'react-konva';
+import React, { useState } from 'react';
+import { Stage, Layer, Line, Rect } from 'react-konva';
 
 function App(props) {
+
+  const [map, setMap] = useState();
 
   let wasm = props.wasm;
 
   function onFileChange(e) {
-    let reader = new FileReader();
-    reader.onloadend = () => {      
-      let v = new Uint8Array(reader.result);
-      let r = wasm.load_map_offsets(v);
-      console.log(r);
+
+    let files = e.target.files;
+    let headerFile;
+    let mapFile;
+
+    for(let i=0;i<e.target.files.length; i++) {
+      if(files[i].name === "GAMEMAPS.WL6") {
+        mapFile = files[i];
+      } else if(files[i].name === "MAPHEAD.WL6") {
+        headerFile = files[i];
+      }
+    }
+
+    let headerReader = new FileReader();    
+    headerReader.onloadend = () => {      
+      let headerData = new Uint8Array(headerReader.result);
+      let offsets = wasm.load_map_offsets(headerData);
+
+      let mapReader = new FileReader();
+      mapReader.onloadend = () => {
+        let mapData = new Uint8Array(mapReader.result);
+        let headers = wasm.load_map_headers(mapData, offsets);
+        let map0 = wasm.load_map(mapData, headers, offsets, 0);
+        setMap(map0);
+      }
+      mapReader.readAsArrayBuffer(mapFile);
     };
-    reader.readAsArrayBuffer(e.target.files[0]);
+
+    headerReader.readAsArrayBuffer(headerFile);  
   }
 
   const dim = Math.min(window.innerWidth, window.innerHeight);
   const gridWidth = dim / 64;
 
-  function buildGrid() {
-    let result = [];
-    for(let x=0;x<=64;x++) {
-      result.push(<Line points={[x*gridWidth, 0, x*gridWidth, dim]} strokeWidth={1} stroke="#f2f2f2" />);
-    }
-    for(let y=0;y<=64;y++) {
-      result.push(<Line points={[0, y*gridWidth, dim, y*gridWidth]} strokeWidth={1} stroke="#f2f2f2" />);
-    }
-    return result;
-  }
+  console.log("rendering");
 
   return (
     // Stage - is a div wrapper
@@ -35,7 +51,10 @@ function App(props) {
     <div>
       <Stage width={dim} height={dim}>
         <Layer>
-          {buildGrid()}
+          {buildGrid(gridWidth, dim)}
+        </Layer>
+        <Layer>
+          {buildWallPlane(gridWidth, map)}
         </Layer>
       </Stage>
       
@@ -44,6 +63,39 @@ function App(props) {
       </div>
     </div>
   );
-}  
+}
+
+function buildGrid(gridWidth, dim) {
+  let result = [];
+  for(let x=0;x<=64;x++) {
+    result.push(<Line key={"x"+x} points={[x*gridWidth, 0, x*gridWidth, dim]} strokeWidth={1} stroke="#f2f2f2" />);
+  }
+  for(let y=0;y<=64;y++) {
+    result.push(<Line key={"y"+y} points={[0, y*gridWidth, dim, y*gridWidth]} strokeWidth={1} stroke="#f2f2f2" />);
+  }
+  return result;
+}
+
+function buildWallPlane(gridWidth, map) {
+  
+  if(!map) {
+    return;
+  }
+
+  console.log(map.segs[0].length);
+
+  let result = [];
+  for(let y=0;y<64;y++) {
+    for(let x=0;x<64;x++) {
+      let ptr = y*64+x;
+      let tile = map.segs[0][ptr];
+      if(tile < 107) { //solid wall tile
+        result.push(<Rect key={x+"|"+y} fill="gray" x={x*gridWidth} y={y*gridWidth} width={gridWidth} height={gridWidth} />);
+      }
+    }
+  }
+  console.log(result.length);
+  return result;
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import init, * as wasm from './pkg/iwlib.js';
+import init, * as wasm from './pkg/libiw.js';
 
 init().then((wasm_loaded) => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
